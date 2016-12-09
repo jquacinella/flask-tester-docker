1b66d2148ad7fe77525010b991411fe4349c1ef7
James Quacinella <james@deemok.com>
2016-12-08 23:15:31 -0500

    Cleanup

--- scripts/prod/create_do_machine.sh
+++ scripts/prod/create_do_machine.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-machine create --driver digitalocean --digitalocean-access-token 4944af0e336ffc55d1698e4ade7645ff205c43fe08f1395c700ac1e1481f7eb0 flask-tester-do
+docker-machine create --driver digitalocean --digitalocean-access-token XXXXXXXXXX flask-tester-do
